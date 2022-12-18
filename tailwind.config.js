f86a44a1f4653ed05bbb3d31124b7e4fa1b68604
dorrion <dhdms8696@naver.com>
2022-12-18 17:51:57 +0900

    [chore] tailwindcss 세팅
    #1

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}", // pages 안에 있는 애들 모두 적용
+    "./components/**/*.{js,ts,jsx,tsx}", // components 안에 있는 애들 모두 적용
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
