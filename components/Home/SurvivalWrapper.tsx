dd52487a312da98fd7c77791617eb35ea07212a1
dorrion <dhdms8696@naver.com>
2022-12-29 02:50:21 +0900

    [✨FEAT] 심리학과에서 살아남기 컴포넌트 분리

---
+++ components/Home/SurvivalWrapper.tsx
@@ -0,0 +1,31 @@
+import { SurvivalProps } from '../../shared/types';
+
+export default function SurvivalWrapper({
+  tipId,
+  title,
+  subtitle,
+  category,
+  tag,
+}: SurvivalProps) {
+  const TagList = tag?.map((el, idx) => (
+    <span key={idx} className="tag">
+      {el}
+    </span>
+  ));
+  return (
+    <>
+      <div className="xl:w-1/4 md:w-1/2 p-4">
+        <div className="bg-gray-100 p-6 rounded-lg">
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+            {title}
+          </h3>
+          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+            {subtitle}
+          </h2>
+          {category && <span className="tag-category">{category}</span>}
+          {TagList}
+        </div>
+      </div>
+    </>
+  );
+}
