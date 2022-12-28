031da757abd746da31899069020dc996f88871fb
dorrion <dhdms8696@naver.com>
2022-12-28 11:30:01 +0900

    [🎨STYLE] 색상 변수 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "jit",
+  mode: 'jit',
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}", // pages 안에 있는 애들 모두 적용
-    "./components/**/*.{js,ts,jsx,tsx}", // components 안에 있는 애들 모두 적용
+    './pages/**/*.{js,ts,jsx,tsx}', // pages 안에 있는 애들 모두 적용
+    './components/**/*.{js,ts,jsx,tsx}', // components 안에 있는 애들 모두 적용
   ],
-  darkMode: "class",
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backgroundColor: '#EDEDED',
+      },
+    },
   },
   plugins: [],
 };
