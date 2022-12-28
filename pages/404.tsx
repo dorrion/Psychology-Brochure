6849cda20010c4e681977462ceb7b795323faea6
dorrion <dhdms8696@naver.com>
2022-12-28 11:30:33 +0900

    [✨FEAT] 404 페이지 추가

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import Image from 'next/image';
+import Ing from 'public/images/제작중.webp';
 
 function PageNotFound() {
-  return <h1>페이지를 찾을 수 없습니다</h1>;
+  return (
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flexBox flex-col">
+          <div className="w-[35rem] h-[35rem] inline-flex items-center justify-center rounded-full bg-slate-100 mb-5 flex-shrink-0">
+            <Image src={Ing} alt="제작 중입니다" width={500} height={500} />
+          </div>
+          <h1 className="sm:text-3xl text-2xl font-bold title-font mb-4 text-gray-900">
+            공사 중입니다! 돌아가주세요!
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default PageNotFound;
