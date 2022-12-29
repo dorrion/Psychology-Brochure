d82c9a636b238eaa33306f9a304ed9233b26d099
dorrion <dhdms8696@naver.com>
2022-12-29 13:03:28 +0900

    [💄DESIGN] 제목 위치 변경

--- components/Home/SurvivalWrapper.tsx
+++ components/Home/SurvivalWrapper.tsx
@@ -17,10 +17,10 @@ export default function SurvivalWrapper({
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-100 p-6 rounded-lg">
           <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
-            {title}
+            {subtitle}
           </h3>
           <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-            {subtitle}
+            {title}
           </h2>
           {category && <span className="tag-category">{category}</span>}
           {TagList}
