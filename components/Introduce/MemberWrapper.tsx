7e281dd3ab53d2173ab5116beef304d0491dcc1d
dorrion <dhdms8696@naver.com>
2022-12-30 01:16:36 +0900

    [✨FEAT] 멤버 리스트 구현

---
+++ components/Introduce/MemberWrapper.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+import { MemberProps } from '../../shared/types';
+
+export default function MemberWrapper({
+  memberId,
+  profileImg,
+  name,
+  role,
+}: MemberProps) {
+  return (
+    <div key={memberId}>
+      <div className="flex flex-wrap -m-4">
+        <div className="p-4 lg:w-1/4 md:w-1/2">
+          <div className="h-full flex flex-col items-center text-center">
+            <div className="bg-soil w-48 h-48  rounded-full">
+              <Image
+                src={profileImg!}
+                className="flex-shrink-0 rounded-lg w-full h-full object-cover object-center mb-4"
+                alt="team"
+                width={200}
+                height={200}
+              />
+            </div>
+            <div className="w-full">
+              <h2 className="title-font font-medium text-xl text-gray-900">
+                {name}
+              </h2>
+
+              {role?.map((el, idx) => (
+                <h3 key={idx} className="text-gray-500 font-light mt-2">
+                  {el}
+                </h3>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
