7b7411935b92b7bede29d6ca2b79f490ab5a3cee
dorrion <dhdms8696@naver.com>
2022-12-30 14:17:24 +0900

    [🔧REFACTOR] 경로 수정

--- components/Home/SurvivalWrapper.tsx
+++ components/Home/SurvivalWrapper.tsx
@@ -1,4 +1,4 @@
-import { SurvivalProps } from '../../shared/types';
+import { SurvivalProps } from 'shared/store/type';
 
 export default function SurvivalWrapper({
   tipId,
@@ -6,20 +6,20 @@ export default function SurvivalWrapper({
   subtitle,
   category,
 }: SurvivalProps) {
+  // 클릭 시 post/:tipId로 라우팅 하는 훅 추가하기
+  // 응 라우팅 아니야~ 모달창이야~
 
   return (
-    <>
-      <div className="xl:w-1/4 md:w-1/2 p-4">
-        <div className="bg-gray-100 p-6 rounded-lg">
-          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
-            {subtitle}
-          </h3>
-          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-            {title}
-          </h2>
-          {category && <span className="tag-category">{category}</span>}
-        </div>
+    <div className="xl:w-1/4 md:w-1/2 p-4">
+      <div className="bg-gray-100 p-6 rounded-lg">
+        <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+          {subtitle}
+        </h3>
+        <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+          {title}
+        </h2>
+        {category && <span className="tag-category">{category}</span>}
       </div>
-    </>
+    </div>
   );
 }
