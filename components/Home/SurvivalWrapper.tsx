02de8e3a0bc5e6678e7507925621dd2d8d05d9ec
dorrion <dhdms8696@naver.com>
2022-12-30 18:20:58 +0900

    [💄DESIGN] 살아남기 디자인 수정

--- components/Home/SurvivalWrapper.tsx
+++ components/Home/SurvivalWrapper.tsx
@@ -1,25 +1,60 @@
 import { SurvivalProps } from 'shared/store/type';
+import { ModalStore } from 'shared/store';
+import Portal from 'shared/components/modal/Modal';
+import Grduated from './Graduated';
+import Image from 'next/image';
 
 export default function SurvivalWrapper({
   tipId,
   title,
   subtitle,
   category,
+  homeImg,
 }: SurvivalProps) {
-  // 클릭 시 post/:tipId로 라우팅 하는 훅 추가하기
-  // 응 라우팅 아니야~ 모달창이야~
+  const { isOpen, setIsOpen } = ModalStore();
 
   return (
-    <div className="xl:w-1/4 md:w-1/2 p-4">
-      <div className="bg-gray-100 p-6 rounded-lg">
-        <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
-          {subtitle}
-        </h3>
-        <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-          {title}
-        </h2>
-        {category && <span className="tag-category">{category}</span>}
+    <>
+      <div onClick={setIsOpen} className="xl:w-1/4 lg:w-1/3 md:w-1/2 p-4">
+        <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <Image
+            src={homeImg!}
+            className="lg:h-48 md:h-36 w-full object-cover object-center mb-4"
+            alt="picture"
+            width={200}
+            height={200}
+          />
+          <div className="p-6">
+            {category && (
+              <h2 className="tracking-widest text-xs title-font font-medium text-yellow-400 mb-1">
+                {category}
+              </h2>
+            )}
+            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+              {title}
+            </h1>
+            <p className="text-gray-600 leading-relaxed mb-3">{subtitle}</p>
+
+            <div className="bottom-0">
+              <a className="text-slate-400 inline-flex items-center md:mb-2 lg:mb-0 ">
+                보기
+                <svg
+                  className="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
