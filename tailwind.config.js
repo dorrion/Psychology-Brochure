6931a98ec7c66b8b2c5cdf97f26c1fba89c9c28b
dorrion <dhdms8696@naver.com>
2022-12-30 16:32:46 +0900

    [🎨STYLE] height 사용자 정의 스타일 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,28 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        backgroundColor: '#EDEDED',
+        'backgroundColor': '#EDEDED',
       },
       backgroundImage: {
         'potato': "url('../public/images/감자배경.webp')",
         'soil':"url('../public/images/땅.webp')"
       },
+      height: {
+        "10v": "10vh",
+        "20v": "20vh",
+        "30v": "30vh",
+        "40v": "40vh",
+        "50v": "50vh",
+        "60v": "60vh",
+        "70v": "70vh",
+        "80v": "80vh",
+        "90v": "90vh",
+        "95v": "95vh",
+        "100v": "100vh",
+      },
     },
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [
+    require("tailwind-scrollbar-hide")
+  ],
 };
