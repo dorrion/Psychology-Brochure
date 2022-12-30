d877d74de38cb772af7c3710c35df3b7d5ec3db9
dorrion <dhdms8696@naver.com>
2022-12-30 19:16:52 +0900

    [🚑FIX] LCP 감지하는 로드 속도 향상

--- components/Home/SurvivalWrapper.tsx
+++ components/Home/SurvivalWrapper.tsx
@@ -1,7 +1,5 @@
 import { SurvivalProps } from 'shared/store/type';
-import { ModalStore } from 'shared/store';
-import Portal from 'shared/components/modal/Modal';
-import Grduated from './Graduated';
+import Link from 'next/link';
 import Image from 'next/image';
 
 export default function SurvivalWrapper({
@@ -11,32 +9,43 @@ export default function SurvivalWrapper({
   category,
   homeImg,
 }: SurvivalProps) {
-  const { isOpen, setIsOpen } = ModalStore();
-
   return (
     <>
-      <div onClick={setIsOpen} className="xl:w-1/4 lg:w-1/3 md:w-1/2 p-4">
+      <div className="xl:w-1/4 lg:w-1/3 md:w-1/2 p-4">
         <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-          <Image
-            src={homeImg!}
-            className="lg:h-48 md:h-36 w-full object-cover object-center mb-4"
-            alt="picture"
-            width={200}
-            height={200}
-          />
-          <div className="p-6">
-            {category && (
-              <h2 className="tracking-widest text-xs title-font font-medium text-yellow-400 mb-1">
-                {category}
-              </h2>
+          <Link href="/post/[id]" as={`/post/${tipId}`}>
+            {homeImg === '/images/홈졸업.webp' ? (
+              // The Largest Contentful Paint 해결
+              <Image
+                src={homeImg!}
+                className="lg:h-48 md:h-36 w-full object-cover object-center mb-4"
+                alt="picture"
+                width={200}
+                height={200}
+                priority={true}
+              />
+            ) : (
+              <Image
+                src={homeImg!}
+                className="lg:h-48 md:h-36 w-full object-cover object-center mb-4"
+                alt="picture"
+                width={200}
+                height={200}
+              />
             )}
-            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-              {title}
-            </h1>
-            <p className="text-gray-600 leading-relaxed mb-3">{subtitle}</p>
 
-            <div className="bottom-0">
-              <a className="text-slate-400 inline-flex items-center md:mb-2 lg:mb-0 ">
+            <div className="p-6">
+              {category && (
+                <h2 className="tracking-widest text-xs title-font font-medium text-yellow-400 mb-1">
+                  {category}
+                </h2>
+              )}
+              <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                {title}
+              </h1>
+              <p className="text-gray-600 leading-relaxed mb-3">{subtitle}</p>
+
+              <div className="text-slate-400 inline-flex items-center md:mb-2 lg:mb-0 ">
                 보기
                 <svg
                   className="w-4 h-4 ml-2"
@@ -50,9 +59,9 @@ export default function SurvivalWrapper({
                   <path d="M5 12h14"></path>
                   <path d="M12 5l7 7-7 7"></path>
                 </svg>
-              </a>
+              </div>
             </div>
-          </div>
+          </Link>
         </div>
       </div>
     </>
