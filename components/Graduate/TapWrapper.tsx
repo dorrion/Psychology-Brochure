ef9c6e24057ceac53e6a1db867b73591caf20a49
dorrion <dhdms8696@naver.com>
2022-12-31 01:50:51 +0900

    [💄DESIGN] Tap 선택 시 스타일 적용

--- components/Graduate/TapWrapper.tsx
+++ components/Graduate/TapWrapper.tsx
@@ -10,38 +10,43 @@ export default function TapWrapper({
   senior,
 }: CurriProps) {
   return (
-    <div>
-      <h1>{major}</h1>
-      <Tab.Group
-        defaultIndex={0}
-        // onChange={(index) => {
-        //   console.log('Changed selected tab to:', index);
-        // }}
-      >
-        <Tab.List>
-          <Tab className="mr-14">1학년</Tab>
-          <Tab className="mr-14">2학년</Tab>
-          <Tab className="mr-14">3학년</Tab>
-          <Tab className="mr-14">4학년</Tab>
-        </Tab.List>
-        <Tab.Panels>
-          <Tab.Panel>
-            <SemesterWrapper spring={freshmen?.spring} fall={freshmen?.fall} />
-          </Tab.Panel>
-          <Tab.Panel>
-            <SemesterWrapper spring={junior?.spring} fall={junior?.fall} />
-          </Tab.Panel>
-          <Tab.Panel>
-            <SemesterWrapper
-              spring={sophomore?.spring}
-              fall={sophomore?.fall}
-            />
-          </Tab.Panel>
-          <Tab.Panel>
-            <SemesterWrapper spring={senior?.spring} fall={senior?.fall} />
-          </Tab.Panel>
-        </Tab.Panels>
-      </Tab.Group>
-    </div>
+    <>
+      <section className="text-gray-600 body-font overflow-hidden">
+        <div className="container px-5 py-24 mx-auto">
+          <h1 className="sm:text-3xl text-2xl title-font font-bold text-gray-900 mt-4 mb-4">
+            {major}
+          </h1>
+          <Tab.Group defaultIndex={0}>
+            <Tab.List>
+              <Tab className="mr-14 text-sm ui-selected:selective">1학년</Tab>
+              <Tab className="mr-14 text-sm ui-selected:selective">2학년</Tab>
+              <Tab className="mr-14 text-sm ui-selected:selective">3학년</Tab>
+              <Tab className="mr-14 text-sm ui-selected:selective">4학년</Tab>
+            </Tab.List>
+            <Tab.Panels>
+              <Tab.Panel>
+                <SemesterWrapper
+                  spring={freshmen?.spring}
+                  fall={freshmen?.fall}
+                />
+              </Tab.Panel>
+              <Tab.Panel>
+                <SemesterWrapper spring={junior?.spring} fall={junior?.fall} />
+              </Tab.Panel>
+              <Tab.Panel>
+                <SemesterWrapper
+                  spring={sophomore?.spring}
+                  fall={sophomore?.fall}
+                />
+              </Tab.Panel>
+              <Tab.Panel>
+                <SemesterWrapper spring={senior?.spring} fall={senior?.fall} />
+              </Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
+      </section>
+      <div></div>
+    </>
   );
 }
