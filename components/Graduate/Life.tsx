2fa6f92c83066dd47b5a1d11294077c0538807c6
dorrion <dhdms8696@naver.com>
2023-01-08 23:58:09 +0900

    [✨FEAT] 전공별 질문 구현

---
+++ components/Graduate/Life.tsx
@@ -0,0 +1,29 @@
+import { MajorProps } from 'shared/store/type';
+
+export default function Interview({ major, explain }: any) {
+  const QuestionList = explain?.map((exp: any, idx: any) => {
+    const AnswerList: JSX.Element[] = exp.answer.map((ans: any, idx: any) => {
+      return (
+        <div key={idx} className="flex items-center mt-4">
+          <div className={`w-14 h-14 circle bg-interview-${ans.color}`}>
+            <div className="text-slate-50 text-xs">{ans.name}</div>
+          </div>
+          <div className="ml-6">{ans.content}</div>
+        </div>
+      );
+    });
+    return (
+      <div key={idx} className="flex flex-col items-start mt-4">
+        <h1 className="text-xl mt-8">{exp.question}</h1>
+        {AnswerList}
+      </div>
+    );
+  });
+
+  return (
+    <div className=" flex flex-col text-left mb-40">
+      <h1 className="text-xl font-bold">{major}</h1>
+      {QuestionList}
+    </div>
+  );
+}
---
+++ pages/graduate/major.tsx
@@ -0,0 +1,44 @@
+import Layout from 'components/layout';
+import Head from 'next/head';
+import { promises as fs } from 'fs';
+import path from 'path';
+import Life from 'components/Graduate/Life';
+import { MajorProps } from 'shared/store/type';
+
+export default function prepare({ GraduateMajorData }: any) {
+  const MajorQnAList: JSX.Element[] = GraduateMajorData.map(
+    (el: MajorProps) => {
+      return <Life key={el.id} major={el.major} explain={el.explain} id={0} />;
+    },
+  );
+
+  return (
+    <Layout>
+      <Head>
+        {/* 메타데이터 */}
+        <title>전공별 질문</title>
+        <meta name="description" content="대학원 준비과정에 대한 인터뷰" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <section className="text-gray-600 body-font bg-slate-200 dark:bg-slate-800">
+        <div className="container px-5 py-24 mx-auto flex-row flex-wrap j w-full mb-20">
+          {MajorQnAList}
+        </div>
+      </section>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  // 살아남기 데이터 경로
+  const filePath = path.join(process.cwd(), 'data', 'GraduateMajor.json');
+  const GraduateMajorData = await fs.readFile(filePath, 'utf8');
+  const objectData = JSON.parse(GraduateMajorData);
+
+  return {
+    props: {
+      GraduateMajorData: await Promise.all(objectData),
+    },
+  };
+}
