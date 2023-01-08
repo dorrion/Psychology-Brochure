b825a35f79ce832207db989d745aa9936a869513
dorrion <dhdms8696@naver.com>
2023-01-08 23:56:30 +0900

    [🔧REFACTOR] 불필요한 코드 삭제

--- components/Graduate/Interview.tsx
+++ components/Graduate/Interview.tsx
@@ -1,15 +1,9 @@
 import { QnAProps } from 'shared/store/type';
 
 export default function Interview({ question, answer }: QnAProps) {
-  const AnswerList = answer?.map((ans: any) => {
+  const AnswerList = answer?.map((ans, idx: any) => {
     return (
-      <div className="flex items-center mt-4">
-        <div className="circle bg-interview-100"></div>
-        <div className="circle bg-interview-200"></div>
-        <div className="circle bg-interview-300"></div>
-        <div className="circle bg-interview-400"></div>
-        <div className="circle bg-interview-500"></div>
-        <div className="circle bg-interview-600"></div>
+      <div key={idx} className={`flex mt-4 items-center`}>
         <div className={`w-14 h-14 circle bg-interview-${ans.color}`}>
           <div className="text-slate-50 text-xs">{ans.name}</div>
         </div>
@@ -18,7 +12,7 @@ export default function Interview({ question, answer }: QnAProps) {
     );
   });
   return (
-    <div className=" text-left">
+    <div className="text-left mb-40">
       <h1 className="text-xl font-bold">{question}</h1>
       {AnswerList}
     </div>
