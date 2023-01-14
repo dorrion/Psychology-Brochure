0595c1f07643ac43ab07f5806f2410a42202b1f7
dorrion <dhdms8696@naver.com>
2023-01-15 06:43:35 +0900

    [✨FEAT] 대학원 생활 추가

--- components/Graduate/Life.tsx
+++ components/Graduate/Life.tsx
@@ -1,29 +1,21 @@
 import { MajorProps } from 'shared/store/type';
 
-export default function Interview({ major, explain }: any) {
-  const QuestionList = explain?.map((exp: any, idx: any) => {
-    const AnswerList: JSX.Element[] = exp.answer.map((ans: any, idx: any) => {
-      return (
-        <div key={idx} className="flex items-center mt-4">
-          <div className={`w-14 h-14 circle bg-interview-${ans.color}`}>
-            <div className="text-slate-50 text-xs">{ans.name}</div>
-          </div>
-          <div className="ml-6">{ans.content}</div>
-        </div>
-      );
-    });
+export default function Interview({ question, answer }: any) {
+  const AnswerList: JSX.Element[] = answer.map((ans: any, idx: any) => {
     return (
-      <div key={idx} className="flex flex-col items-start mt-4">
-        <h1 className="text-xl mt-8">{exp.question}</h1>
-        {AnswerList}
+      <div key={idx} className="flex items-center mt-4">
+        <div className={`w-14 h-14 circle bg-interview-${ans.color}`}>
+          <div className="text-slate-50 text-xs">{ans.name}</div>
+        </div>
+        <p className="ml-6">{ans.content}</p>
       </div>
     );
   });
 
   return (
     <div className=" flex flex-col text-left mb-40">
-      <h1 className="text-xl font-bold">{major}</h1>
-      {QuestionList}
+      <h1 className="text-xl font-bold">{question}</h1>
+      {AnswerList}
     </div>
   );
 }
