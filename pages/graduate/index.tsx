2668e0f6fea3c25b8147e8da0f67019c288cdad2
dorrion <dhdms8696@naver.com>
2023-01-25 23:12:54 +0900

    [💄DESIGN] 배너 타이틀 다크모드 적용

--- pages/graduate/index.tsx
+++ pages/graduate/index.tsx
@@ -35,19 +35,21 @@ const Tabs = ({ CurriData }: any) => {
         <div className="container px-5 py-24 mx-auto">
           <div className="w-full h-32 bg-shadowColor rounded-lg flex mb-9 relative overflow-hidden">
             <div className="flex flex-col justify-center ml-8">
-              <h1 className="text-xl font-bold mb-2">추천 커리큘럼</h1>
-              <h2 className="text-base">
+              <h1 className="text-xl font-bold mb-2 dark:text-slate-800">
+                추천 커리큘럼
+              </h1>
+              <h2 className="text-base dark:text-slate-800">
                 세부 전공 별 추천 이수 과목을 알려드려요
               </h2>
             </div>
-            {/* layout 적용해서 컨테이너 사이즈 따라 이미지 사이즈 변경되도록 하기 */}
+            {/* 반응형 위해 줄어들면 이미지는 없어지도록 */}
             <Image
               className="absolute -bottom-14 right-0"
               src="/images/대학원커리.webp"
               width={534}
               height={212}
               alt="대학원 커리큘럼 이미지"
-              layout="intrinsic"
+              layout="full"
             />
           </div>
           <GraduateHeader />
