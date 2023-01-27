eff0a4fe1a41c8a54b0b2cff5510a454c6a5735a
Oheun Kim <dhdms8696@naver.com>
2023-01-27 23:35:39 +0900

    Revert "#8 사용자가 대학원 메뉴 클릭 시 배너를 변경한다." (#23)

--- pages/graduate/index.tsx
+++ pages/graduate/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import path from 'path';
 import { promises as fs } from 'fs';
 import Layout from 'components/layout';
+import { Tab } from '@headlessui/react';
 import { CurriProps } from 'shared/store/type';
 import TapWrapper from 'components/Graduate/TapWrapper';
 import GraduateHeader from 'components/Graduate/GraduateHeader';
@@ -33,25 +34,6 @@ const Tabs = ({ CurriData }: any) => {
       </Head>
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
-          <div className="w-full h-32 bg-shadowColor rounded-lg flex mb-9 relative overflow-hidden">
-            <div className="flex flex-col justify-center ml-8">
-              <h1 className="text-xl font-bold mb-2 dark:text-slate-800">
-                추천 커리큘럼
-              </h1>
-              <h2 className="text-base dark:text-slate-800">
-                세부 전공 별 추천 이수 과목을 알려드려요
-              </h2>
-            </div>
-            {/* 반응형 위해 줄어들면 이미지는 없어지도록 */}
-            <Image
-              className="absolute -bottom-14 right-0 hidden lg:block"
-              src="/images/대학원커리.webp"
-              width={534}
-              height={212}
-              alt="대학원 커리큘럼 이미지"
-              layout="full"
-            />
-          </div>
           <GraduateHeader />
           {TapWrap}
         </div>
