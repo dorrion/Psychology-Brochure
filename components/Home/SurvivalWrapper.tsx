60fb4dcfd30a8846b846b26b4b738952ca9793bb
dorrion <dhdms8696@naver.com>
2023-01-28 22:07:21 +0900

    [🚑FIX] 보기 위치 변경

--- components/Home/SurvivalWrapper.tsx
+++ components/Home/SurvivalWrapper.tsx
@@ -13,7 +13,7 @@ export default function SurvivalWrapper({
   return (
     <>
       <div className="xl:w-1/4 lg:w-1/3 md:w-1/2 p-4">
-        <div className="h-full dark:bg-slate-700 bg-gray-50 overflow-hidden">
+        <div className="h-full dark:bg-slate-700 bg-gray-50 overflow-hidden relative">
           <Link href="/post/[id]" as={`/post/${tipId}`}>
             {homeImg === '/images/홈졸업.webp' ? (
               // The Largest Contentful Paint 해결
@@ -35,7 +35,7 @@ export default function SurvivalWrapper({
               />
             )}
 
-            <div className="p-6">
+            <div className="p-4 mb-4">
               {category && (
                 <h2 className="tracking-widest text-xs title-font font-medium text-primaryColor dark:text-secondaryColor mb-1">
                   {category}
@@ -45,11 +45,10 @@ export default function SurvivalWrapper({
                 {title}
               </h1>
               <p className="text-gray-600 leading-relaxed mb-3">{subtitle}</p>
-
-              <div className="flexBox text-slate-400 inline-flex items-center md:mb-2 lg:mb-0 ">
-                보기
-                <ArrowRightIcon />
-              </div>
+            </div>
+            <div className="absolute bottom-4 left-4 flexBox text-slate-400 inline-flex items-center md:mb-2 lg:mb-0 ">
+              보기
+              <ArrowRightIcon />
             </div>
           </Link>
         </div>
