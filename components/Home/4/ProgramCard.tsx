4fa301a88085c64a0b52cf8b0781ff9da88e4a11
dorrion <dhdms8696@naver.com>
2023-01-30 21:16:58 +0900

    [✨FEAT] 강의형 더보기 기능 구현

--- components/Home/4/ProgramCard.tsx
+++ components/Home/4/ProgramCard.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Image from 'next/image';
 import { ProgramProps } from 'shared/store/type';
 
@@ -9,24 +10,42 @@ export default function ProgramCard({
   period,
 }: ProgramProps) {
   // category로 분기처리해서 색깔 하나 지정하고 가자
+  let color;
+  if (category === '강의형') {
+    color = 'secondaryColor';
+  } else if (category === '세미나') {
+    color = 'primaryColor';
+  } else {
+    color = 'interview-200';
+  }
+
   return (
     <div
-      className="flex-col flexBox m-8 w-60 h-80 border-[1px]
-     border-shadowColor rounded-2xl text-center p-5"
+      className="flex-col flex justify-start items-center m-8 w-60 h-80 border-[1px]
+     border-shadowColor rounded-2xl text-center p-5 relative"
     >
       <div
         className={
-          'w-20 h-20 flex-shrink-0 circle font-bold text-slate-50' +
-          (category === '교내장학' ? ' bg-secondaryColor' : ' bg-primaryColor')
+          'mt-9 w-20 h-20 flex-shrink-0 circle font-bold text-slate-50 bg-' +
+          color
         }
       >
         {category}
       </div>
       <h2 className="text-base font-bold mt-5">{name}</h2>
-      <h3 className="text-xs mt-1.5">{description}</h3>
-      <p className="w-40 h-px bg-shadowColor my-4" />
-      <h3 className="text-md font-medium">{area}</h3>
-      <h3 className="text-md font-medium">{period}</h3>
+      <h3 className="text-xs mt-1.5 whitespace-pre-wrap">{description}</h3>
+
+      {/* 영역, 모집시기는 왼쪽 정렬 */}
+      <div className="absolute bottom-4 left-4">
+        <div className="flex">
+          <h3 className={`text-sm text-${color}`}>영역 </h3>
+          <h3 className="text-sm">{area}</h3>
+        </div>
+        <div className="flex">
+          <h3 className={`text-sm text-${color}`}>모집시기 </h3>
+          <h3 className="text-sm">{period}</h3>
+        </div>
+      </div>
     </div>
   );
 }
