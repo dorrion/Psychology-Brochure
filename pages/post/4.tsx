4d630dff993820bea95ff88ef7ba3228bfa42b21
dorrion <dhdms8696@naver.com>
2023-01-30 21:23:27 +0900

    [✨FEAT] 세미나형, 연수형 컴포넌트 추가

--- pages/post/4.tsx
+++ pages/post/4.tsx
@@ -1,30 +1,63 @@
 import Head from 'next/head';
 import Layout from 'components/layout';
-import { Check } from 'components/icon';
-import { Lecture } from 'data/Data';
+import { Lecture, Seminar, Camp } from 'data/Data';
 import ProgramCard from 'components/Home/4/ProgramCard';
 import { useState } from 'react';
 import { DownArrow, UpArrow } from 'components/icon';
 
 export default function index() {
-  // 교내 더보기&접기 상태 저장
-  const [isSchoolMoreView, setIsSchoolMoreView] = useState<Boolean>(false);
+  // 강의형
+  const [isLectureMoreView, setIsLectureMoreView] = useState<Boolean>(false);
   const onClickMoreViewButton = () => {
-    setIsSchoolMoreView(!isSchoolMoreView);
-  }; // 클릭시 상태 반전
+    setIsLectureMoreView(!isLectureMoreView);
+  };
 
-  // 교외 더보기&접기 상태 저장
-  const [isSuburbsMoreView, setIsSuburbsMoreView] = useState<Boolean>(false);
-  const onClickSuburbsMoreViewButton = () => {
-    setIsSuburbsMoreView(!isSuburbsMoreView);
-  }; // 클릭시 상태 반전
+  // 세미나형
+  const [isSeminarMoreView, setIsSeminarMoreView] = useState<Boolean>(false);
+  const onClickSeminarMoreViewButton = () => {
+    setIsSeminarMoreView(!isSeminarMoreView);
+  };
+
+  // 캠프형
+  const [isCampMoreView, setIsCampMoreView] = useState<Boolean>(false);
+  const onClickCampMoreViewButton = () => {
+    setIsCampMoreView(!isCampMoreView);
+  };
 
   // 더보기 버튼이 True일 때는 모두 보여주기
   // 더보기 버튼이 False일 때는 3개만 slice 해서 보여주기
-  let SchoolMoney = isSchoolMoreView ? Lecture : Lecture.slice(0, 3);
+  let LectureList = isLectureMoreView ? Lecture : Lecture.slice(0, 3);
+  let SeminarList = isSeminarMoreView ? Seminar : Seminar.slice(0, 3);
+  let CampList = isCampMoreView ? Camp : Camp.slice(0, 3);
 
-  // 교내 장학
-  const school = SchoolMoney?.map((el) => {
+  // 강의형
+  const lecture = LectureList?.map((el) => {
+    return (
+      <ProgramCard
+        key={el.name}
+        name={el.name}
+        description={el.description}
+        category={el.category}
+        area={el.area}
+        period={el.period}
+      />
+    );
+  });
+  // 세미나형
+  const seminar = SeminarList?.map((el) => {
+    return (
+      <ProgramCard
+        key={el.name}
+        name={el.name}
+        description={el.description}
+        category={el.category}
+        area={el.area}
+        period={el.period}
+      />
+    );
+  });
+  // 캠프, 연수형
+  const camp = CampList?.map((el) => {
     return (
       <ProgramCard
         key={el.name}
@@ -48,23 +81,33 @@ export default function index() {
       </Head>
       <section className="text-gray-600 body-font ">
         <div className="container px-5 py-24 mx-auto flex-col flexBox">
-          <div className="flex flex-wrap">{school}</div>
+          <div className="flex flex-wrap">{lecture}</div>
           <button className="flex-col flexBox" onClick={onClickMoreViewButton}>
             <p className="mb-4 font-base">
-              {isSchoolMoreView ? '접기' : '더보기'}
+              {isLectureMoreView ? '접기' : '더보기'}
             </p>
-            {isSchoolMoreView ? <UpArrow /> : <DownArrow />}
+            {isLectureMoreView ? <UpArrow /> : <DownArrow />}
           </button>
-          {/* <div className="flex flex-wrap">{suburbs}</div>
+          <div className="flex flex-wrap">{seminar}</div>
           <button
             className="flex-col flexBox"
-            onClick={onClickSuburbsMoreViewButton}
+            onClick={onClickSeminarMoreViewButton}
           >
             <p className="mb-4 font-base">
-              {isSuburbsMoreView ? '접기' : '더보기'}
+              {isSeminarMoreView ? '접기' : '더보기'}
             </p>
-            {isSuburbsMoreView ? <UpArrow /> : <DownArrow />}
-          </button> */}
+            {isSeminarMoreView ? <UpArrow /> : <DownArrow />}
+          </button>
+          <div className="flex flex-wrap">{camp}</div>
+          <button
+            className="flex-col flexBox"
+            onClick={onClickCampMoreViewButton}
+          >
+            <p className="mb-4 font-base">
+              {isCampMoreView ? '접기' : '더보기'}
+            </p>
+            {isCampMoreView ? <UpArrow /> : <DownArrow />}
+          </button>
         </div>
       </section>
     </Layout>
