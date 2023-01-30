a22167223d1b182822335f331ba4212c47cc26aa
dorrion <dhdms8696@naver.com>
2023-01-31 01:57:09 +0900

    [🚑FIX] 충돌 해결

--- components/Graduate/FreeCard.tsx
+++ components/Graduate/FreeCard.tsx
@@ -2,7 +2,9 @@ import { EnrolementProps } from 'shared/store/type';
 
 export default function FreeCard({ idx, name, tip }: EnrolementProps) {
   return (
-    <div className={`col-span-1 h-full p-4 ${idx % 3 === 2 ? '' : 'border-r'}`}>
+    <div
+      className={`col-span-1 h-full p-4 ${idx! % 3 === 2 ? '' : 'border-r'}`}
+    >
       <p className="text-xs">{tip}</p>
       <p className="text-xs font-bold mt-1">{name}</p>
     </div>
