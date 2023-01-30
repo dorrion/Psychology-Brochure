fc14863c10f69bfa3a4368c515f8bc344abad339
dorrion <dhdms8696@naver.com>
2023-01-31 01:04:21 +0900

    [💄DESIGN] 제목, 내용 사이에 마진 추가

--- components/Home/6/EnrolementCard.tsx
+++ components/Home/6/EnrolementCard.tsx
@@ -4,7 +4,7 @@ import { EnrolementProps } from 'shared/store/type';
 export default function EnrolementCard({ name, tip }: EnrolementProps) {
   return (
     <div className="w-48 bg-white rounded-lg shadow-lg overflow-hidden p-4 mb-4 last:mb-0">
-      <h1 className="text-xs font-medium">{name}</h1>
+      <h1 className="text-xs font-medium mb-2">{name}</h1>
       <p className="whitespace-pre-wrap text-xs">{tip}</p>
     </div>
   );
