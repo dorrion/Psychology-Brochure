bf20ce444bcaedbb1b43074e8286b423a89ab077
dorrion <dhdms8696@naver.com>
2023-01-31 01:52:36 +0900

    [✨FEAT] 자유 발언 컴포넌트 구현

---
+++ components/Graduate/FreeCard.tsx
@@ -0,0 +1,10 @@
+import { EnrolementProps } from 'shared/store/type';
+
+export default function FreeCard({ idx, name, tip }: EnrolementProps) {
+  return (
+    <div className={`col-span-1 h-full p-4 ${idx % 3 === 2 ? '' : 'border-r'}`}>
+      <p className="text-xs">{tip}</p>
+      <p className="text-xs font-bold mt-1">{name}</p>
+    </div>
+  );
+}
