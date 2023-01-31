403170b4527935a1dfc6ee5a7ffd319d6c3e5ac4
Oheun Kim <dhdms8696@naver.com>
2023-01-31 19:56:01 +0900

    [✨FEAT] 대학원 입시요강 페이지를 제작한다 (#39)

--- pages/graduate/guide.tsx
+++ pages/graduate/guide.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import path from 'path';
-import { promises as fs } from 'fs';
 import Layout from 'components/layout';
 import GraduateHeader from 'components/Graduate/GraduateHeader';
 
-const Tabs = () => {
+const Guide = () => {
+  interface Tab {
+    id?: string;
+    label?: string;
+    content?: React.ReactNode;
+  }
+
+  interface Props {
+    tabs: Tab[];
+  }
+
+  const Tabs: React.FC<Props> = ({ tabs }) => {
+    const [activeTab, setActiveTab] = useState(tabs[0].id);
+
+    return (
+      <>
+        {/* 탭 나열 */}
+        <ul className="flexBox flex-shrink-0 border-gray-200">
+          {tabs.map((tab) => (
+            <li
+              key={tab.id}
+              // 선택된 탭이면 배경색 바뀌도록
+              className={`m-4 w-56 h-12 p-4 flexBox text-base text-center cursor-pointer rounded-2xl border ${
+                activeTab === tab.id ? 'bg-shadowColor' : ''
+              }`}
+              onClick={() => setActiveTab(tab.id)}
+            >
+              {tab.label}
+            </li>
+          ))}
+        </ul>
+        <div className="p-6">
+          {/* 현재 선택된 탭의 content 삽입 */}
+          {tabs.find((tab) => tab.id === activeTab)?.content}
+        </div>
+      </>
+    );
+  };
   return (
     <Layout>
       <Head>
@@ -54,23 +89,82 @@ const Tabs = () => {
             />
           </div>
           <GraduateHeader />
+          <Tabs
+            tabs={[
+              {
+                id: '0',
+                label: '서울대학교 모집요강',
+                content: (
+                  <>
+                    <h1 className="text-xl mb-2.5">일반대학원, 교육대학원</h1>
+                    <embed
+                      className="w-full  h-[768px]"
+                      src="/pdf/서울대(일,교).pdf"
+                      type="application/pdf"
+                    />
+                  </>
+                ),
+              },
+              {
+                id: '1',
+                label: '고려대학교 모집요강',
+                content: (
+                  <>
+                    <h1 className="text-xl mb-2.5">일반대학원, 교육대학원</h1>
+                    <embed
+                      className="w-full  h-[768px]"
+                      src="/pdf/고려대(일,교).pdf"
+                      type="application/pdf"
+                    />
+                  </>
+                ),
+              },
+              {
+                id: '2',
+                label: '연세대학교 모집요강',
+                content: (
+                  <>
+                    <h1 className="text-xl mb-2.5">일반대학원</h1>
+                    <embed
+                      className="w-full  h-[768px] mb-12"
+                      src="/pdf/연세대(일).pdf"
+                      type="application/pdf"
+                    />
+                    <h1 className="text-xl mb-2.5">교육대학원</h1>
+                    <embed
+                      className="w-full  h-[768px]"
+                      src="/pdf/연세대(교).pdf"
+                      type="application/pdf"
+                    />
+                  </>
+                ),
+              },
+              {
+                id: '3',
+                label: '아주대학교 모집요강',
+                content: (
+                  <>
+                    <h1 className="text-xl mb-2.5">일반대학원</h1>
+                    <embed
+                      className="w-full  h-[768px] mb-12"
+                      src="/pdf/아주대(일).pdf"
+                      type="application/pdf"
+                    />
+                    <h1 className="text-xl mb-2.5">교육대학원</h1>
+                    <embed
+                      className="w-full  h-[768px]"
+                      src="/pdf/아주대(교).pdf"
+                      type="application/pdf"
+                    />
+                  </>
+                ),
+              },
+            ]}
+          />
         </div>
       </section>
     </Layout>
   );
 };
 
-export default Tabs;
-
-export async function getStaticProps() {
-  // 커리큘럼
-  const filePath = path.join(process.cwd(), 'data', 'GraduateCrc.json');
-  const CurriData = await fs.readFile(filePath, 'utf8');
-  const objectData = JSON.parse(CurriData);
-
-  return {
-    props: {
-      CurriData: await Promise.all(objectData),
-    },
-  };
-}
+export default Guide;
