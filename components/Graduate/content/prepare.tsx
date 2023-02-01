79b25a0844820d45cc1609b0342b6b07c7d20a29
dorrion <dhdms8696@naver.com>
2023-02-02 03:51:31 +0900

    [🔧REFACTOR] 대학원 준비 코드 리팩토링

--- components/Graduate/content/prepare.tsx
+++ components/Graduate/content/prepare.tsx
@@ -1,29 +1,10 @@
-import Layout from 'components/layout';
-import Head from 'next/head';
-import Image from 'next/image';
 import Interview from 'components/Graduate/Interview';
 import { QnAProps } from 'shared/store/type';
-import GraduateHeader from 'components/Graduate/GraduateHeader';
 
 export default function prepare({ GraduatePrepareData }: any) {
-  const InterviewList: JSX.Element[] = GraduatePrepareData.map(
-    (QnA: QnAProps) => {
-      return (
-        <Interview key={QnA.id} question={QnA.question} answer={QnA.answer} />
-      );
-    },
-  );
-
-  return (
-    <Layout>
-      <Head>
-        {/* 메타데이터 */}
-        <title>대학원 준비</title>
-        <meta name="description" content="대학원 준비과정에 대한 인터뷰" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      {InterviewList}
-    </Layout>
-  );
+  return GraduatePrepareData.map((QnA: QnAProps) => {
+    return (
+      <Interview key={QnA.id} question={QnA.question} answer={QnA.answer} />
+    );
+  });
 }
