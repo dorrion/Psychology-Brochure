100c2e1adaebf14bcd630436cba879eccba6694d
dorrion <dhdms8696@naver.com>
2023-02-01 20:48:13 +0900

    [💄DESIGN] 스크롤 높이 수정

--- components/Home/5/Five.tsx
+++ components/Home/5/Five.tsx
@@ -3,9 +3,11 @@ import { EnrolementProps } from 'shared/store/type';
 
 export default function FiveCard({ name, tip }: EnrolementProps) {
   return (
-    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4 bg-white rounded-lg shadow-lg overflow-hidden p-4 mx-2 mb-4 last:mb-0">
+    <div className="overflow-y-visible w-full sm:w-full md:w-1/2 lg:w-1/3 bg-white rounded-lg shadow-lg p-6 mx-2 mb-4 last:mb-0">
       <h1 className="dark:text-slate-700 text-xs font-medium mb-2">{name}</h1>
-      <p className="dark:text-slate-700 whitespace-pre-wrap text-xs">{tip}</p>
+      <p className="h-full dark:text-slate-700 whitespace-pre-wrap text-xs ">
+        {tip}
+      </p>
     </div>
   );
 }
