35fdfc8a4ec0b1f643b340371e059179f341e7ad
dorrion <dhdms8696@naver.com>
2023-02-04 17:59:31 +0900

    [💄DESIGN] 부제목 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,15 +30,14 @@ export default function Home({ data }: any) {
         <section className="text-gray-600 body-font bg-slate-200 dark:bg-slate-800">
           <div className="container px-5 py-24 mx-auto ">
             <div className="flex-col flex-wrap flexBox w-full mb-20">
-              <div className="lg:w-1/2 w-full mb-6 lg:mb-0 ">
+              <div className="lg:w-1/2 w-full flexBox flex-col mb-6 lg:mb-0 ">
                 <h1 className="text-center sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900 dark:text-gray-100">
                   심리학과에서 살아남기
                 </h1>
-                {/* <div className="h-1 w-20 my-2 mx-auto bg-indigo-500 rounded"></div> */}
+                <p className="text-center w-full leading-relaxed text-gray-500">
+                  아주대학교 심리학과 학생들에게 유용한 정보를 소개합니다.
+                </p>
               </div>
-              <p className="text-center lg:w-1/2 w-full leading-relaxed text-gray-500">
-                아주대학교 심리학과 학생들에게 유용한 정보를 소개합니다.
-              </p>
             </div>
             <div className="flex flex-wrap -m-4">{TipList}</div>
           </div>
