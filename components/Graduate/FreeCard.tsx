b91dffa2ea4789f85f93a3b4b429eb406f9ee80a
dorrion <dhdms8696@naver.com>
2023-02-04 23:28:00 +0900

    [💄DESIGN] 자유 발언 폰트 적용

--- components/Graduate/FreeCard.tsx
+++ components/Graduate/FreeCard.tsx
@@ -5,7 +5,7 @@ export default function FreeCard({ idx, name, tip }: EnrolementProps) {
     <div
       className={`col-span-1 h-full p-4 ${idx! % 3 === 2 ? '' : 'border-r'}`}
     >
-      <p className="text-base">{tip}</p>
+      <p className="text-base font-extralight">{tip}</p>
       <p className="text-base font-bold mt-1">{name}</p>
     </div>
   );
