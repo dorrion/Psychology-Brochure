f1413e990f70bb57b9ab56284b64f17872b4f063
dorrion <dhdms8696@naver.com>
2023-02-04 21:33:08 +0900

    [🔧REFACTOR] Summary 분리

---
+++ components/Home/1/Summary.tsx
@@ -0,0 +1,52 @@
+import {
+  BookIcon,
+  ScoreIcon,
+  EnglishIcon,
+  SemesterIcon,
+} from 'components/icon';
+
+export default function Summary() {
+  return (
+    <div className="flexBox flex-wrap">
+      <div className="flexBox text-center">
+        <div className="w-12 h-12 flexBox circle bg-slate-100 mb-5 flex-shrink-0">
+          <BookIcon />
+        </div>
+        <div className="flex flex-col">
+          <p className="text-sm font-normal">최소 이수 학점</p>
+          <div className="flex justify-center items-center">
+            <p className="text-2xl font-extrabold">120 </p>
+            <p className="text-sm font-normal">학점</p>
+          </div>
+        </div>
+      </div>
+      <div className="flexBox text-center">
+        <div className="w-12 h-12 flexBox circle bg-slate-100 mb-5 flex-shrink-0">
+          <ScoreIcon />
+        </div>
+        <div>
+          <p className="text-sm font-normal">최소 평점</p>
+          <p className="text-2xl font-bold">평점 2.0</p>
+        </div>
+      </div>
+      <div className="flexBox text-center">
+        <div className="w-12 h-12 flexBox circle bg-slate-100 mb-5 flex-shrink-0">
+          <EnglishIcon />
+        </div>
+        <div>
+          <p className="text-sm font-normal">공인어학점수</p>
+          <p className="text-2xl font-bold">730 Toeic</p>
+        </div>
+      </div>
+      <div className="flexBox text-center">
+        <div className="w-12 h-12 flexBox circle bg-slate-100 mb-5 flex-shrink-0">
+          <SemesterIcon />
+        </div>
+        <div>
+          <p className="text-sm font-normal">최소 이수 학기</p>
+          <p className="text-2xl font-bold">6학기</p>
+        </div>
+      </div>
+    </div>
+  );
+}
