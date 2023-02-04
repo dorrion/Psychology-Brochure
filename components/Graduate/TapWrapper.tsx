99581f23f973344923b3bee9556f09e7ce8300c7
dorrion <dhdms8696@naver.com>
2023-02-04 22:53:12 +0900

    [💄DESIGN] 커리큘럼 탭에 폰트 사이즈, 굵기 변경

--- components/Graduate/TapWrapper.tsx
+++ components/Graduate/TapWrapper.tsx
@@ -17,10 +17,18 @@ export default function TapWrapper({
         </h1>
         <Tab.Group defaultIndex={0}>
           <Tab.List className="dark:text-slate-400">
-            <Tab className="mr-14 text-sm ui-selected:selective">1학년</Tab>
-            <Tab className="mr-14 text-sm ui-selected:selective">2학년</Tab>
-            <Tab className="mr-14 text-sm ui-selected:selective">3학년</Tab>
-            <Tab className="mr-14 text-sm ui-selected:selective">4학년</Tab>
+            <Tab className="mr-14 text-lg font-medium ui-selected:selective">
+              1학년
+            </Tab>
+            <Tab className="mr-14 text-lg font-medium ui-selected:selective">
+              2학년
+            </Tab>
+            <Tab className="mr-14 text-lg font-medium ui-selected:selective">
+              3학년
+            </Tab>
+            <Tab className="mr-14 text-lg font-medium ui-selected:selective">
+              4학년
+            </Tab>
           </Tab.List>
           <Tab.Panels>
             <Tab.Panel>
