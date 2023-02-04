66fc3d78fdc61e45b345b851eb205981f00048be
dorrion <dhdms8696@naver.com>
2023-02-04 23:38:23 +0900

    [🔧REFACTOR] 취업 모바일 반응형 적용

--- components/Career/InterviewWrapper.tsx
+++ components/Career/InterviewWrapper.tsx
@@ -11,26 +11,24 @@ export default function InterviewWrapper({
 }: CarrerProps) {
   return (
     <>
-      <div className="lg:w-1/4 md:w-1/3 sm:w-1/2 m-4">
-        <div className="drop-shadow-xl h-96 dark:bg-slate-700 bg-gray-50 overflow-hidden relative">
+      <div className="w-full md:w-1/3 lg:w-1/4 m-4">
+        <div className="h-[285px] drop-shadow-xl dark:bg-slate-700 bg-gray-50 overflow-hidden relative">
           <Link href={`/carrer/$[tipId]`} as={`/career/${tipId}`}>
             <Image
               src={carrerImg!}
-              className="h-48 sm:w-full object-cover object-center mb-4"
+              className="h-32 w-full object-cover object-center mb-4"
               alt="picture"
               width={232}
               height={140}
             />
 
             <div className="p-4 mb-4 md:mb-12">
-              <h1 className="text-2xl font-medium text-gray-900 mb-3">
-                {title}
-              </h1>
-              <p className="whitespace-pre text-gray-600 leading-relaxed mb-3">
+              <h1 className="text-2xl font-normal text-gray-900">{title}</h1>
+              <p className="hidden sm:block whitespace-pre text-base font-extralight text-gray-600 leading-relaxed mb-3">
                 {subtitle}
               </p>
             </div>
-            <div className="absolute bottom-4 left-4 flexBox text-slate-400 inline-flex items-center md:mb-2 lg:mb-0 ">
+            <div className="absolute text-base font-extralight bottom-4 left-4 flexBox text-slate-400 inline-flex items-center md:mb-2 lg:mb-0 ">
               보기
               <ArrowRightIcon />
             </div>
