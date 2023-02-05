1bf93327b183df83a1908253eaed2f7bb8e5cdf6
dorrion <dhdms8696@naver.com>
2023-02-06 00:01:24 +0900

    [✨FEAT] 공군장교 프로필

--- pages/career/1.tsx
+++ pages/career/1.tsx
@@ -15,8 +15,8 @@ export default function Home({ data }: any) {
         </Head>
 
         <section className="text-gray-600 body-font">
-          {/* 프로필 */}{' '}
-          <div className="container px-5 py-24 mx-auto flexBox flex-col">
+          {/* 프로필 */}
+          <div className="container py-12 mx-auto flex flexBox">
             <Image
               className="w-60 h-60 circle"
               src="/images/취업공군프로필.webp"
@@ -24,6 +24,24 @@ export default function Home({ data }: any) {
               height={250}
               alt="대학원 커리큘럼 이미지"
             />
+            {/* 소개란 */}
+            <div className="ml-12">
+              <h2 className="text-2xl font-light">
+                "대한민국 영공을 수호하는 강력한 힘"
+              </h2>
+
+              <h1 className="text-5xl font-black mb-5">참새</h1>
+              <div className="flex">
+                <p className="text-base font-extrabold">직업군</p>
+                <p className="text-base font-extralight ml-3.5">
+                  교육/법률/사회복지/경찰/소방직 및 군인
+                </p>
+              </div>
+              <div className="flex">
+                <p className="text-base font-extrabold">직업명</p>
+                <p className="text-base font-extralight ml-3.5">공군 장교</p>
+              </div>
+            </div>
           </div>
         </section>
       </Layout>
