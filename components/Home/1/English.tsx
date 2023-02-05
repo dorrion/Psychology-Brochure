137e11d23dc596bbe5a0680b9390e01fbb05a713
dorrion <dhdms8696@naver.com>
2023-02-05 20:43:46 +0900

    [💄DESIGN] 졸업하기 모바일 반응형 및 폰트 수정

--- components/Home/1/English.tsx
+++ components/Home/1/English.tsx
@@ -6,7 +6,7 @@ export default function English() {
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto flexBox flex-col">
           <h1 className="text-4xl font-bold  text-center">외국어 공인 성적</h1>
-          <div className="flex-row flexBox mt-16">
+          <div className="flex flex-wrap flexBox mt-16">
             <EnglishCard english="TOEIC" score="730" />
             <EnglishCard english="TEPS" score="329" />
             <EnglishCard english="TOEFL" score="" />
