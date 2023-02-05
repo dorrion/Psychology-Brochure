b140b53748cbd6b5933010eafba86fb358151e4f
dorrion <dhdms8696@naver.com>
2023-02-06 01:04:30 +0900

    [🚑FIX] key 에러 해결

--- components/Career/JobInterview.tsx
+++ components/Career/JobInterview.tsx
@@ -1,15 +1,15 @@
 export default function Interview({ name, color, interview }: any) {
   return interview.map((el: any, idx: any) => {
     return (
-      <>
+      <div key={idx}>
         <h1 className="text-2xl font-medium mt-10">{el.question}</h1>
-        <div key={idx} className="flex items-center mt-4">
+        <div className="flex items-center mt-4">
           <div className={`w-14 h-14 circle bg-interview-${color}`}>
             <div className="text-slate-50 text-lg font-normal">{name}</div>
           </div>
           <p className="text-lg font-extralight ml-6">{el.answer}</p>
         </div>
-      </>
+      </div>
     );
   });
 }
