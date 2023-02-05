e105e13e4529393681f5178ba09e053e7a9a8ec2
dorrion <dhdms8696@naver.com>
2023-02-05 23:17:38 +0900

    [🚑FIX] 헤더 보이는 에러 수정

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -4,32 +4,35 @@ import DarkmodeToggleButton from './darkmode-toggle-button';
 export default function Header() {
   return (
     <>
-      <header className="text-gray-600 body-font fixed top-0 left-0 right-0 bg-slate-50 dark:bg-slate-800">
+      <header className="text-gray-600 body-font top-0 left-0 right-0 bg-slate-50 dark:bg-slate-800">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <Link
             href="/"
             className="flex font-medium items-center text-gray-900 mb-4 md:mb-0"
           >
-            <span className="ml-3 text-xl font-bold">아늑</span>
+            <span className="ml-3 text-4xl font-bold">아늑</span>
           </Link>
           <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-            <Link className="mr-5 hover:text-gray-900" href="/introduce">
+            <Link
+              className="mr-5 text-xl hover:text-gray-900"
+              href="/introduce"
+            >
               소개
             </Link>
-            <Link href="/graduate" className="mr-5 hover:text-gray-900">
+            <Link href="/graduate" className="mr-5 text-xl hover:text-gray-900">
               대학원
             </Link>
-            <Link href="/career" className="mr-5 hover:text-gray-900">
+            <Link href="/career" className="mr-5 text-xl hover:text-gray-900">
               취업
             </Link>
-            <Link href="/abroad" className="mr-5 hover:text-gray-900">
+            <Link href="/abroad" className="mr-5 text-xl hover:text-gray-900">
               유학
             </Link>
-            <Link href="/tool" className="mr-5 hover:text-gray-900">
+            <Link href="/tool" className="mr-5 text-xl hover:text-gray-900">
               도구
             </Link>
           </nav>
-          <DarkmodeToggleButton />
+            <DarkmodeToggleButton />
         </div>
       </header>
     </>
