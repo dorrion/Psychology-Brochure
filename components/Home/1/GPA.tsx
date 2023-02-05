2e310b047984920415a0e213172bcfeca3e3a90e
dorrion <dhdms8696@naver.com>
2023-02-05 20:12:59 +0900

    [💄DESIGN] 총 평점 평균 폰트 및 크기 수정

--- components/Home/1/GPA.tsx
+++ components/Home/1/GPA.tsx
@@ -3,10 +3,12 @@ export default function GPA() {
     <>
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto flexBox flex-col">
-          <h1 className="text-4xl font-bold  text-center">총 평점 평균</h1>
-          <div className="mt-16 w-52 h-52 rounded-full flex-col flexBox flex-shrink-0 drop-shadow-xl bg-customGreen">
-            <h1 className="text-8xl text-white font-bold text-center">2.0</h1>
-            <h1 className="text-5xl text-white font-bold text-center">학점</h1>
+          <h1 className="text-4xl font-black text-center">총 평점 평균</h1>
+          <div className="mt-12 w-52 h-52 rounded-full flex-col flexBox flex-shrink-0 drop-shadow-xl bg-customGreen">
+            <h1 className="text-[3.125rem] text-white font-black text-center">
+              2.0
+            </h1>
+            <h1 className="text-4xl text-white font-bold text-center">학점</h1>
           </div>
         </div>
       </section>
