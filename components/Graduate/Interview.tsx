b214d3d16149ef2c58137903c818f92aebb6da6e
dorrion <dhdms8696@naver.com>
2023-02-06 21:16:02 +0900

    [💄DESIGN] 조건부 폰트크기 적용

--- components/Graduate/Interview.tsx
+++ components/Graduate/Interview.tsx
@@ -10,8 +10,14 @@ export default function Interview({ question, answer }: QnAProps) {
         <div className="w-14 h-14 circle bg-interview-400" />
         <div className="w-14 h-14 circle bg-interview-500" />
         <div className="w-14 h-14 circle bg-interview-600" /> */}
-        <div className={`w-10 h-10 circle bg-interview-${ans.color}`}>
-          <div className="text-slate-50 text-xl font-normal">{ans.name}</div>
+        <div className={`flexBox w-10 h-10 circle bg-interview-${ans.color}`}>
+          <div
+            className={`text-slate-50 font-normal ${
+              ans.name.length > 3 ? 'text-[0.5rem]' : 'text-base'
+            }`}
+          >
+            {ans.name}
+          </div>
         </div>
         <div className="text-lg font-extralight ml-6">{ans.content}</div>
       </div>
