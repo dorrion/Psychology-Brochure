16449d7002b3d8737d07c648c347247209aab4ea
Oheun Kim <dhdms8696@naver.com>
2023-02-06 22:12:10 +0900

    [🚑FIX] 대학원 페이지를 수정한다 (#98)

--- components/Graduate/Interview.tsx
+++ components/Graduate/Interview.tsx
@@ -2,6 +2,8 @@ import { QnAProps } from 'shared/store/type';
 
 export default function Interview({ question, answer }: QnAProps) {
   const AnswerList = answer?.map((ans, idx: any) => {
+    let ansName = ans.name;
+    let length = ansName?.length;
     return (
       <div key={idx} className={`flex mt-4 items-center`}>
         {/* <div className="w-14 h-14 circle bg-interview-100" />
@@ -10,8 +12,14 @@ export default function Interview({ question, answer }: QnAProps) {
         <div className="w-14 h-14 circle bg-interview-400" />
         <div className="w-14 h-14 circle bg-interview-500" />
         <div className="w-14 h-14 circle bg-interview-600" /> */}
-        <div className={`w-14 h-14 circle bg-interview-${ans.color}`}>
-          <div className="text-slate-50 text-xl font-normal">{ans.name}</div>
+        <div className={`flexBox w-10 h-10 circle bg-interview-${ans.color}`}>
+          <div
+            className={`text-slate-50 font-normal ${
+              length && length > 3 ? 'text-[0.5rem]' : 'text-base'
+            }`}
+          >
+            {ans.name}
+          </div>
         </div>
         <div className="text-lg font-extralight ml-6">{ans.content}</div>
       </div>
--- components/Graduate/Major.tsx
+++ components/Graduate/Major.tsx
@@ -5,8 +5,16 @@ export default function Interview({ major, explain }: any) {
     const AnswerList: JSX.Element[] = exp.answer.map((ans: any, idx: any) => {
       return (
         <div key={idx} className="flex items-center mt-5">
-          <div className={`w-14 h-14 circle bg-interview-${ans.color}`}>
-            <div className="text-slate-50 text-base">{ans.name}</div>
+          <div className={`w-10 h-10 circle bg-interview-${ans.color}`}>
+            <div
+              className={`text-slate-50 font-normal ${
+                ans.name.length && ans.name.length > 3
+                  ? 'text-[0.5rem]'
+                  : 'text-base'
+              }`}
+            >
+              {ans.name}
+            </div>
           </div>
           <div className="ml-6 text-lg font-extralight">{ans.content}</div>
         </div>
