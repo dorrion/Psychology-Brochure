6c0493528f74274c25ceb3ae38f14f2948209c09
dorrion <dhdms8696@naver.com>
2023-02-06 21:08:19 +0900

    [💄DESIGN] 프로필 크기 수정

--- components/Graduate/Interview.tsx
+++ components/Graduate/Interview.tsx
@@ -10,7 +10,7 @@ export default function Interview({ question, answer }: QnAProps) {
         <div className="w-14 h-14 circle bg-interview-400" />
         <div className="w-14 h-14 circle bg-interview-500" />
         <div className="w-14 h-14 circle bg-interview-600" /> */}
-        <div className={`w-14 h-14 circle bg-interview-${ans.color}`}>
+        <div className={`w-10 h-10 circle bg-interview-${ans.color}`}>
           <div className="text-slate-50 text-xl font-normal">{ans.name}</div>
         </div>
         <div className="text-lg font-extralight ml-6">{ans.content}</div>
