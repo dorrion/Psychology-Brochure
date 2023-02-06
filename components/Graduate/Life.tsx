5d4b439d00b3033c7b10381ff8a114621fc0cbdc
dorrion <dhdms8696@naver.com>
2023-02-06 22:02:52 +0900

    [💄DESIGN] 프로필 크기 축소

--- components/Graduate/Life.tsx
+++ components/Graduate/Life.tsx
@@ -1,11 +1,17 @@
-import { MajorProps } from 'shared/store/type';
-
 export default function Interview({ question, answer }: any) {
   const AnswerList: JSX.Element[] = answer.map((ans: any, idx: any) => {
     return (
       <div key={idx} className="flex items-center mt-4">
-        <div className={`w-14 h-14 circle bg-interview-${ans.color}`}>
-          <div className="text-slate-50 text-lg font-normal">{ans.name}</div>
+        <div className={`w-10 h-10 circle bg-interview-${ans.color}`}>
+          <div
+            className={`text-slate-50 font-normal ${
+              ans.name.length && ans.name.length > 3
+                ? 'text-[0.5rem]'
+                : 'text-base'
+            }`}
+          >
+            {ans.name}
+          </div>
         </div>
         <p className="text-lg font-extralight ml-6">{ans.content}</p>
       </div>
