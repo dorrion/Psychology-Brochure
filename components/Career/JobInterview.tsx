b290aa07c8a07fcf78364dbaf686217c8fd50603
Oheun Kim <dhdms8696@naver.com>
2023-02-07 22:56:11 +0900

    [💄DESIGN] 취업 인터뷰 프로필 줄인다 (#110)

--- components/Career/JobInterview.tsx
+++ components/Career/JobInterview.tsx
@@ -4,8 +4,8 @@ export default function Interview({ name, color, interview }: any) {
       <div key={idx}>
         <h1 className="text-xl sm:text-2xl font-medium mt-10">{el.question}</h1>
         <div className="flex items-center mt-4">
-          <div className={`w-14 h-14 circle bg-interview-${color}`}>
-            <div className="text-slate-50 text-lg font-normal">{name}</div>
+          <div className={`w-10 h-10 circle bg-interview-${color}`}>
+            <div className="text-slate-50 text-base font-normal">{name}</div>
           </div>
           <p className="text-lg font-extralight ml-6">{el.answer}</p>
         </div>
