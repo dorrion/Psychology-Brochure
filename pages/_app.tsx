2d70d6503768200e9ccf465f8cb53c027dda004d
Oheun Kim <dhdms8696@naver.com>
2023-02-18 16:58:52 +0900

    [✨FEAT] 뷰저블 코드를 적용한다 (#141)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -44,6 +44,23 @@ function MyApp({ Component, pageProps }: AppProps) {
           `,
         }}
       />
+      {/* 뷰저블 코드 */}
+      <Script
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `
+              (function(w, d, a){
+                w.__beusablerumclient__ = {
+                  load : function(src){
+                    var b = d.createElement("script");
+                    b.src = src; b.async=true; b.type = "text/javascript";
+                    d.getElementsByTagName("head")[0].appendChild(b);
+                  }
+                };w.__beusablerumclient__.load(a + "?url=" + encodeURIComponent(d.URL));
+              })(window, document, "//rum.beusable.net/load/b230218e164817u247");
+            `,
+        }}
+      />
       {/* GA 설정 끝 */}
       <Component {...pageProps} />
       {/* Vercel Analytics */}
